2531405d413b92f9f99f151e9824b5ad125c8604
zckoh <zckoh96@gmail.com>
2019-03-09 11:16:37 +0000

    Added positional encoding (standard addition), and positional encoding (concatenation)

--- Embedding.py
+++ Embedding.py
@@ -2,8 +2,12 @@
 # Date Created: 08-Mar-2019 4:38:59 pm
 # Author: zckoh
 # Description: Embedding method before input to encoder.
+#              Includes basic embedding, positional encoding,
+#              and concatenating positional encoding.
 import torch
 import torch.nn as nn
+import math
+from torch.autograd import Variable
 
 class Embedder(nn.Module):
     """
@@ -16,13 +20,49 @@ class Embedder(nn.Module):
         self.embed = nn.Embedding(vocab_size, d_model)
 
     def forward(self, x):
-        return self.embed(x)
+        # make embeddings relatively larger
+        return self.embed(x) * math.sqrt(self.d_model)
 
 class PositionalEncoder(nn.Module):
-    def __init__(self, d_model):
+    "Implement the PE function with addition."
+    def __init__(self, d_model, dropout = 0.1, max_seq_len = 1024):
         super().__init__()
-        # TODO: Add positional sinusoid
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(max_seq_len, d_model)
+        position = torch.arange(0., max_seq_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0., d_model, 2) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
 
     def forward(self, x):
-        # TODO: Add forward pass
-        return
+        x = x + Variable(self.pe[:, :x.size(1)], requires_grad=False)
+        return self.dropout(x)
+
+class PositionalEncoderConcat(nn.Module):
+    "Implement the PE function with concatenation istead."
+    "Output dimension will be (1,N,d_model*2)"
+    def __init__(self, d_model, dropout = 0.0, max_seq_len = 1024):
+        super().__init__()
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(max_seq_len, d_model)
+        position = torch.arange(0., max_seq_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0., d_model, 2) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # Concatenate embeddings with positional sinusoid
+        pe = Variable(self.pe[:, :x.size(1)], requires_grad=False)
+        x = torch.cat((x,pe),2)
+
+        return self.dropout(x)
