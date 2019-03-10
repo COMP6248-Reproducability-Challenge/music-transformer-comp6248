47a3ed5f3cbe7828ed2a247d2d5cd85a5f7f7d64
Jin <jhy3g15@soton.ac.uk>
2019-03-10 22:28:02 +0000

    Added Feed-forward sublayer

---
+++ PositionalFeedForward.py
@@ -0,0 +1,21 @@
+"""
+Created on Sun Mar 10 21:58:37 2019
+
+@author: jhy3g15
+"""
+
+import torch.nn as nn
+import torch.nn.Functional as F
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=2048, dropout = 0.1):
+        super().__init__()
+        # d_ff defaulted to 2048
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.linear_1(x)))
+        x = self.linear_2(x)
+        return x
